d094926904e4dd987129478063b42981944687af
Bernarda Brkić <bernarda.brkic1@gmail.com>
2023-01-30 18:12:01 +0100

    Added custom questions.

--- src/Questions.js
+++ src/Questions.js
@@ -1,42 +1,160 @@
 export const questions = [
   {
     id: "q1",
-    questionText: "What is the capital of France?",
+    questionText:
+      "Koje godine je osnovano poduzeće koje danas poznajemo kao Kraš?",
     answerOptions: [
-      { answerText: "New York", isCorrect: false },
-      { answerText: "London", isCorrect: false },
-      { answerText: "Paris", isCorrect: true },
-      { answerText: "Dublin", isCorrect: false },
+      { answerText: "1920.", isCorrect: false },
+      { answerText: "1911.", isCorrect: true },
+      { answerText: "1953.", isCorrect: false },
+      { answerText: "1931.", isCorrect: false },
     ],
   },
   {
     id: "q2",
-    questionText: "Who is CEO of Tesla?",
+    questionText: "Pod kojim nazivom je osnovano poduzeće?",
     answerOptions: [
-      { answerText: "Jeff Bezos", isCorrect: false },
-      { answerText: "Elon Musk", isCorrect: true },
-      { answerText: "Bill Gates", isCorrect: false },
-      { answerText: "Tony Stark", isCorrect: false },
+      { answerText: "Kraš", isCorrect: false },
+      { answerText: "Kandit", isCorrect: false },
+      { answerText: "Bonbonierre", isCorrect: false },
+      { answerText: "Union", isCorrect: true },
     ],
   },
   {
     id: "q3",
-    questionText: "The iPhone was created by which company?",
+    questionText: "Tko su dva osnivača poduzeća proizvodnje slastica?",
     answerOptions: [
-      { answerText: "Apple", isCorrect: true },
-      { answerText: "Intel", isCorrect: false },
-      { answerText: "Amazon", isCorrect: false },
-      { answerText: "Microsoft", isCorrect: false },
+      { answerText: "König i Deutsch", isCorrect: true },
+      { answerText: "Hönigsberg i Deutsch", isCorrect: false },
+      { answerText: "Fellner i Helmer", isCorrect: false },
+      { answerText: "König i Helmer", isCorrect: false },
     ],
   },
   {
     id: "q4",
-    questionText: "How many Harry Potter books are there?",
+    questionText:
+      "Za koji kraljevski dvor je tvornica postala dvorski dobavljač?",
     answerOptions: [
-      { answerText: "1", isCorrect: false },
-      { answerText: "4", isCorrect: false },
-      { answerText: "6", isCorrect: false },
-      { answerText: "7", isCorrect: true },
+      { answerText: "Dvor u Parizu", isCorrect: false },
+      { answerText: "Dvor u Sankt Peterburgu", isCorrect: false },
+      { answerText: "Dvor u Beču i Budimu", isCorrect: true },
+      { answerText: "Dvor u Berlinu", isCorrect: false },
+    ],
+  },
+  {
+    id: "q5",
+    questionText: "Koje godine započinje proizvodnja karamela i deserata?",
+    answerOptions: [
+      { answerText: "1915.", isCorrect: false },
+      { answerText: "1920.", isCorrect: true },
+      { answerText: "1925.", isCorrect: false },
+      { answerText: "1930.", isCorrect: false },
+    ],
+  },
+  {
+    id: "q6",
+    questionText:
+      "Koji mlin za proizvodnju brašna 1921. godine postaje tvornica za proizvodnju keksa, vafla i slanica?",
+    answerOptions: [
+      { answerText: "Viktorija", isCorrect: false },
+      { answerText: "Elizabeta", isCorrect: false },
+      { answerText: "Karolina", isCorrect: true },
+      { answerText: "Terezija", isCorrect: false },
+    ],
+  },
+  {
+    id: "q7",
+    questionText:
+      "Koji je poznati dječji pisac osmislio slogan za Ki-Ki „Bilo kuda Ki-Ki svuda“?",
+    answerOptions: [
+      { answerText: "Ratko Zvrko", isCorrect: true },
+      { answerText: "Zvonimir Balog", isCorrect: false },
+      { answerText: "Grigor Vitez", isCorrect: false },
+      { answerText: "Miro Gavran", isCorrect: false },
+    ],
+  },
+  {
+    id: "q8",
+    questionText:
+      "Koje godine je započela proizvodnja antologijskog bombona s crtom?",
+    answerOptions: [
+      { answerText: "1925.", isCorrect: false },
+      { answerText: "1926.", isCorrect: false },
+      { answerText: "1935.", isCorrect: false },
+      { answerText: "1936.", isCorrect: true },
+    ],
+  },
+  {
+    id: "q9",
+    questionText:
+      "Proizvodnja preteče kojeg čokoladnog proizvoda započinje 1938. godine?",
+    answerOptions: [
+      { answerText: "Dorina", isCorrect: false },
+      { answerText: "Životinjsko carstvo", isCorrect: true },
+      { answerText: "Čoksa", isCorrect: false },
+      { answerText: "Bananko", isCorrect: false },
+    ],
+  },
+  {
+    id: "q10",
+    questionText:
+      "Koja Kraševa bombonijera je poznata po slojevima svog presjeka?",
+    answerOptions: [
+      { answerText: "Bajadera", isCorrect: true },
+      { answerText: "Griotte", isCorrect: false },
+      { answerText: "Fontana", isCorrect: false },
+      { answerText: "Dorina Praline", isCorrect: false },
+    ],
+  },
+  {
+    id: "q11",
+    questionText:
+      "Kako se naziva antologijsko čokoladirano čajno pecivo, čija je proizvodnja započela 1957. godine?",
+    answerOptions: [
+      { answerText: "Pravnica", isCorrect: false },
+      { answerText: "Domaćica", isCorrect: true },
+      { answerText: "Programerka", isCorrect: false },
+      { answerText: "Umjetnica", isCorrect: false },
+    ],
+  },
+  {
+    id: "q12",
+    questionText: "Kako se naziva Krašev kakao napitak?",
+    answerOptions: [
+      { answerText: "Kraš Nitro", isCorrect: false },
+      { answerText: "Kraš Turbo", isCorrect: false },
+      { answerText: "Kraš Diesel", isCorrect: false },
+      { answerText: "Kraš Express", isCorrect: true },
+    ],
+  },
+  {
+    id: "q13",
+    questionText: "Slogan kojeg proizvoda je „Prijatelj iz djetinjstva“?",
+    answerOptions: [
+      { answerText: "Bananko", isCorrect: false },
+      { answerText: "Dorina", isCorrect: false },
+      { answerText: "505", isCorrect: true },
+      { answerText: "Domaćica", isCorrect: false },
+    ],
+  },
+  {
+    id: "q14",
+    questionText: "Slogan bombonijere Griotte glasi:",
+    answerOptions: [
+      { answerText: "Umjetnost zavođenja", isCorrect: true },
+      { answerText: "Satkana užitkom", isCorrect: false },
+      { answerText: "Lakše se diše", isCorrect: false },
+      { answerText: "Obiteljska miljenica", isCorrect: false },
+    ],
+  },
+  {
+    id: "q15",
+    questionText: "Slogan kompanije Kraš glasi:",
+    answerOptions: [
+      { answerText: "Samo je jedan Kraš", isCorrect: false },
+      { answerText: "Okus koji spaja generacije ", isCorrect: true },
+      { answerText: "Bilo kuda Kraš svuda", isCorrect: false },
+      { answerText: "Kraš, uvijek u vašem domu", isCorrect: false },
     ],
   },
 ];
